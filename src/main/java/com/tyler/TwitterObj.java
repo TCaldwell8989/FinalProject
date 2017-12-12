d4681f87b9a3fba8e6039dde091e88894136cd5a
TCaldwell89 <xw7020pn@go.minneapolis.edu>
2017-12-12 11:48:24 -0600

    Twitter Bot - Final Project

--- src/main/java/com/tyler/TwitterObj.java
+++ src/main/java/com/tyler/TwitterObj.java
@@ -4,13 +4,7 @@ import twitter4j.Twitter;
 import twitter4j.TwitterFactory;
 import twitter4j.conf.ConfigurationBuilder;
 
-/* Class handling my Twitter configuration and authentication
-   CREATE Environmental Variables for the following
-   TWITTER_CONSUMER_KEY = 30DJiSLT0Z0rV1iLW197BlrND
-   TWITTER_CONSUMER_SECRET = H2BBP8twdOCoWj0PB8XzOSYtX4Su14iIL92DYNHAttqDbPEzJ5
-   TWITTER_ACCESS_TOKEN = 937904976835248129-memMeN3WG4mppPH0QHWd4qvCiZgN0fp
-   TWITTER_ACCESS_TOKEN_SECRET = heklosMa0qrP4EfzT50QmfE7qyUOxAr43ub1sT12B5JS9
-*/
+// Class handling my Twitter configuration and authentication
 
 class TwitterObj {
     String consumerKey;
